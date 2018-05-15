d3ae48ed547aaca547aaa606432251180de3a7d2
Ebineson <ebinesoni@gmail.com>
2018-05-15 14:59:48 +0530

    push without post heroko

--- src/server.js
+++ src/server.js
@@ -1,17 +1,9 @@
-
-//Install express server
+//server.js
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+const path = require('path');
+app.use(express.static('./dist/ebineson-app'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join('./dist/ebineson-app/index.html'));
 });
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
