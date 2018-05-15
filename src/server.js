4544634fe1817791cf7414bd06ae7f215701d506
Ebineson <ebinesoni@gmail.com>
2018-05-15 14:23:50 +0530

    push without post heroko

--- src/server.js
+++ src/server.js
@@ -4,10 +4,10 @@ const app = express();
 
 const path = require('path');
 
-app.use(express.static('./dist/ebineson-app'));
+app.use(express.static('/dist/ebineson-app'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join('./dist/ebineson-app/index.html'));
+  res.sendFile(path.join('/dist/ebineson-app/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
